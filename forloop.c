3fa0ec46fc3e01f439c86a6eeea96bbf62f4fbd6
Nitish Jha <nitishkumar0914@gmail.com>
2023-10-17 23:47:48 +0530

    if-else, looping

---
+++ forloop.c
@@ -0,0 +1,10 @@
+#include "stdio.h"
+int main(){
+    
+    for(int i=1; i<=11; i++){     //first go to initialising, then condition check, then inside block,then increment or decrement 
+        printf("inside loop %d\n",i);  //%d for integers, %f for floating no
+
+    }
+
+
+}
---
+++ ifelse.c
@@ -0,0 +1,13 @@
+#include "stdio.h"
+int main()
+{
+    if (-1)   //0 means false all any values other than 0 return true
+    {
+        printf("Condition true");
+    }
+    else
+    {
+        printf("condition false");
+    }
+    return 0;
+}
